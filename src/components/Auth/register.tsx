9153c1881fbbb97216fe04d600dc411e041ee856
Pablo Hdez <pheralb@DESKTOP-6BNJL06.localdomain>
2021-12-21 13:15:20 +0000

    🔨 Fix homepage

---
+++ src/components/Auth/register.tsx
@@ -0,0 +1,234 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { API_URL } from "services/settings";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Button,
+  Text,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  Collapse,
+  FormErrorMessage,
+  useDisclosure,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { useTranslation } from "react-i18next";
+import Show from "animations/Show";
+import confetti from "canvas-confetti";
+
+interface IFormInput {
+  username: string;
+  email: string;
+  password: string;
+}
+
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [t, i18n] = useTranslation("global");
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const bgColor = useColorModeValue("white", "#161618");
+  const borderColor = useColorModeValue("#E2E8F0", "#3B3B3D");
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>();
+
+  const handleRegister = () => {
+    setError(null);
+    setLoading(true);
+    axios
+      .post(`${API_URL}/user/register`, {
+        username: username,
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        setLoading(false);
+        onOpen();
+        confetti();
+      })
+      .catch((error) => {
+        setLoading(false);
+        if (error.response.status === 400) {
+          setError(error.response.data.message);
+        } else {
+          console.log(error);
+        }
+      });
+  };
+
+  const onSubmit = () => {
+    handleRegister();
+  };
+
+  return (
+    <>
+      <Flex align={"center"} justify={"center"}>
+        <Stack spacing={2} mx={"auto"} maxW={"lg"}>
+          <Show delay={0}>
+            <Text fontSize={"4xl"}>{t("authPage.titleSignIn")}</Text>
+          </Show>
+          <Show delay={0.1}>
+            <Box rounded={"lg"} p={8} border="1px" borderColor={borderColor}>
+              <Stack spacing={4}>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <Collapse in={isOpen} animateOpacity>
+                    <Box
+                      p="20px"
+                      color="black"
+                      mb="5"
+                      bg="#FFBD59"
+                      rounded="md"
+                    >
+                      <Text fontSize="20px">🎉 ¡Bienvenido a Podbuster!</Text>
+                      <Text mb="2">
+                        Tu cuenta ha sido creada correctamente.
+                      </Text>
+                      <Link to="/auth">
+                        <Button
+                          variant="ghost"
+                          fontWeight="light"
+                          border="1px"
+                          w="100%"
+                          color="black"
+                          borderColor="black"
+                        >
+                          {t("buttons.LogIn")}
+                        </Button>
+                      </Link>
+                    </Box>
+                  </Collapse>
+                  <FormControl id="username" isInvalid={!!errors?.username}>
+                    <FormLabel>{t("authPage.Username")}</FormLabel>
+                    <Input
+                      id="username"
+                      {...register("username", {
+                        required: `${t("validateRequired.Username")}`,
+                      })}
+                      variant="filled"
+                      borderColor={borderColor}
+                      type="text"
+                      placeholder={t("authPage.Username")}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <FormErrorMessage>
+                      {errors.username && errors.username.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                  <FormControl id="email" mt="16px" isInvalid={!!errors?.email}>
+                    <FormLabel>{t("authPage.Email")}</FormLabel>
+                    <Input
+                      id="email"
+                      {...register("email", {
+                        required: `${t("validateRequired.Email")}`,
+                        pattern: {
+                          value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                          message: "Enter a valid email address",
+                        },
+                      })}
+                      variant="filled"
+                      borderColor={borderColor}
+                      type="email"
+                      placeholder={t("authPage.Email")}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <FormErrorMessage>
+                      {errors.email && errors.email.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                  <FormControl
+                    id="password"
+                    mt="16px"
+                    isInvalid={!!errors?.password}
+                  >
+                    <FormLabel>{t("authPage.Password")}</FormLabel>
+                    <Input
+                      id="password"
+                      type="password"
+                      {...register("password", {
+                        required: `${t("validateRequired.Password")}`,
+                        minLength: {
+                          value: 6,
+                          message: `${t(
+                            "validateRequired.PasswordCharacters"
+                          )}`,
+                        },
+                      })}
+                      variant="filled"
+                      borderColor={borderColor}
+                      placeholder={t("authPage.Password")}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <FormErrorMessage>
+                      {errors.password && errors.password.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                  <Stack spacing={5} mt="16px">
+                    {loading ? (
+                      <Button
+                        isLoading
+                        loadingText="Loading"
+                        spinnerPlacement="start"
+                        border="1px"
+                        borderColor={borderColor}
+                        fontWeight="light"
+                      >
+                        Loading...
+                      </Button>
+                    ) : (
+                      <Button
+                        border="1px"
+                        borderColor={borderColor}
+                        fontWeight="light"
+                        type="submit"
+                      >
+                        {t("buttons.SignIn")}
+                      </Button>
+                    )}
+                    {error && (
+                      <Alert status="error" borderRadius={5}>
+                        <AlertIcon />
+                        <AlertTitle mr={2} fontWeight="light">
+                          {error}
+                        </AlertTitle>
+                      </Alert>
+                    )}
+                  </Stack>
+                </form>
+              </Stack>
+              <Flex mt="5">
+                <Text>{t("authPage.AlreadyAcc")}</Text>
+                <Link to="/auth/login">
+                  <Button
+                    colorScheme="teal"
+                    variant="link"
+                    ml="2"
+                    fontWeight="light"
+                  >
+                    {t("buttons.LogIn")}
+                  </Button>
+                </Link>
+              </Flex>
+            </Box>
+          </Show>
+        </Stack>
+      </Flex>
+    </>
+  );
+};
+
+export default Register;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,92 +2,67 @@ import React from "react";
 import {
   chakra,
   Box,
+  GridItem,
   useColorModeValue,
   Button,
   Stack,
-  Image,
-  Text,
+  Center,
+  Flex,
   Icon,
+  SimpleGrid,
+  VisuallyHidden,
+  Input,
 } from "@chakra-ui/react";
 
 import { Helmet } from "react-helmet";
 import ShowPodcasts from "components/Podcasts/showPodcasts";
 import Show from "animations/Show";
 import { Link } from "react-router-dom";
+import RegisterForm from "components/Auth/register";
 
 const Index = () => {
+  const color = useColorModeValue("black", "white");
+  const bgHeader = useColorModeValue("#CDB285", "#4A4A4A");
+  const borderColor = useColorModeValue("#E2E8F0", "#3B3B3D");
   return (
     <>
       <Helmet>
         <title>Podcasts - Podbuster</title>
       </Helmet>
-      <Box px={8} py={16}>
-        <Box
-          w={{ base: "full", md: 11 / 12, xl: 9 / 12 }}
+      <Box
+        px={8}
+        py={5}
+        mx="auto"
+        bg={bgHeader}
+        borderBottom="1px"
+        borderColor={borderColor}
+      >
+        <SimpleGrid
+          alignItems="center"
+          w={{ base: "full", xl: 11 / 12 }}
+          columns={{ base: 1, lg: 11 }}
           mx="auto"
-          textAlign={{ base: "center", md: "center" }}
         >
-          <Show delay={0}>
-            <chakra.h1
-              mb={6}
-              fontSize={{ base: "4xl", md: "6xl" }}
-              fontWeight="bold"
-            >
-              Escucha{" "}
-              <Text
-                display={{ base: "block", lg: "inline" }}
-                w="full"
-                bgClip="text"
-                bgGradient="linear(to-r, orange.400,#FFBD59)"
-                fontWeight="extrabold"
-              >
-                podcasts
-              </Text>{" "}
-              dónde quieras, cuando quieras.
-            </chakra.h1>
-          </Show>
-          <Stack
-            direction={{ base: "column", sm: "row" }}
-            mb={{ base: 4, md: 8 }}
-            spacing={2}
-            justifyContent={{ sm: "left", md: "center" }}
+          <GridItem
+            colSpan={{ base: "auto", lg: 7 }}
+            textAlign={{ base: "center", lg: "left" }}
           >
-            <Button
-              as="a"
-              variant="solid"
-              colorScheme="brand"
-              display="inline-flex"
-              alignItems="center"
-              justifyContent="center"
-              w={{ base: "full", sm: "auto" }}
-              mb={{ base: 2, sm: 0 }}
-              size="lg"
-              cursor="pointer"
+            <chakra.h1
+              mb={4}
+              fontSize={{ base: "3xl", md: "5xl" }}
+              fontWeight="bold"
+              lineHeight={{ base: "shorter", md: "none" }}
+              color={color}
             >
-              Get Started
-              <Icon boxSize={4} ml={1} viewBox="0 0 20 20" fill="currentColor">
-                <path
-                  fillRule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </Icon>
-            </Button>
-            <Button
-              as="a"
-              colorScheme="gray"
-              display="inline-flex"
-              alignItems="center"
-              justifyContent="center"
-              w={{ base: "full", sm: "auto" }}
-              mb={{ base: 2, sm: 0 }}
-              size="lg"
-              cursor="pointer"
-            >
-              Book a Demo
-            </Button>
-          </Stack>
-        </Box>
+              Escucha podcasts dónde quieras, cuando quieras.
+            </chakra.h1>
+          </GridItem>
+          <GridItem colSpan={{ base: "auto", md: 4 }}>
+            <Box mb={6} p="2">
+              <RegisterForm />
+            </Box>
+          </GridItem>
+        </SimpleGrid>
       </Box>
       <Box p="3">
         <ShowPodcasts />
